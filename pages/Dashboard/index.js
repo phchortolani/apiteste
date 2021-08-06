847e76f763918f9162cab026ebd0d19080bded74
unknown <phchortolani@gmail.com>
2021-08-06 16:40:08 -0300

    Add blur in close side bar

--- pages/Dashboard/index.js
+++ pages/Dashboard/index.js
@@ -42,7 +42,7 @@ export default function Dashboard(props) {
                         </ul>
                     </div>
                 </div>
-                <div onClick={() => setNavOpen((navopen ? false : ""))} className={"main-panel " + (navopen ? "blur" : "")} id="main-panel">
+                <div onClick={() => { navopen ? setNavOpen(false) : "" }} className={"main-panel " + (navopen ? "blur" : "")} id="main-panel">
 
                     <nav className={"navbar navbar-expand-lg   bg-primary  navbar-absolute  " + (optionsopen ? "bg-white" : "navbar-transparent")}>
                         <div className="container-fluid">
