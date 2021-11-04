d5c259be76c7ed699d2e67e6d388a89fe8c89824
unknown <phchortolani@gmail.com>
2021-11-04 09:20:34 -0300

    ajustesapi

--- pages/api/obterBlog.js
+++ pages/api/obterBlog.js
@@ -2,7 +2,7 @@ export default async (request, response) => {
 
     const url = "https://graph.instagram.com/me/media?access_token="
         + process.env.INSTA_TOKEN +
-        "&fields=media_url,media_type,caption,permalink,timestamp,thumbnail_url,id,username,children{media_url},full_name";
+        "&fields=media_url,media_type,caption,permalink,timestamp,thumbnail_url,id,username,children{media_url}";
 
     const chamada = await fetch(url)
         .then(async function (response) {
