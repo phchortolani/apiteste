e612ee4fc5ebf80494a89073fc88856568393442
phchortolani <phchortolani@gmail.com>
2022-02-23 18:53:01 -0300

    ajustes ste

--- pages/ste.js
+++ pages/ste.js
@@ -47,7 +47,7 @@ export default function Ste(props) {
         <>
 
             <NavBar topScreen={topScreen} />
-            <div id="page-container" /* className="fade" */>
+            <div id="page-container" className="bg-background-land" /* className="fade" */>
                 <div id="home" className="content has-bg home gradientar" data-aos="fade-in">
                     <div className="content-bg">
                     </div>
@@ -55,9 +55,9 @@ export default function Ste(props) {
                         <h1 data-aos="zoom-in-down" data-aos-duration="3000" >Dara Marques</h1>
                         <h3 data-aos="zoom-in-left" data-aos-duration="3000" data-aos-delay="1500"  >Construindo um dia a dia com mais maturidade!</h3>
 
-                        <button className="btn  btn-theme btn-primary" data-aos="zoom-in-up" data-aos-delay="2000" data-aos-duration="2000"> Autoagendamento</button>
+                        {/*    <button className="btn  btn-theme btn-primary" data-aos="zoom-in-up" data-aos-delay="2000" data-aos-duration="2000"> Autoagendamento</button> */}
 
-                        <button className="btn btn-theme btn-success" data-aos="zoom-in-up" data-aos-delay="2000" data-aos-duration="2000"> <i className="fab fa-whatsapp fa-lg fa-fw"></i> Fale direto no WhatsApp</button>
+                        <button className="btn btn-theme btn-success" data-aos="zoom-in-up" data-aos-delay="2000" data-aos-duration="2000"> <i className="fab fa-whatsapp fa-lg fa-fw"></i> Agende direto pelo WhatsApp</button>
 
 
                         {/* <a href="#" className="btn btn-theme btn-outline-white">Redes Sociais</a><br /> */}
@@ -66,11 +66,10 @@ export default function Ste(props) {
                 </div>
 
 
-                <div id="about" className="content mt-0" data-scrollview="true" data-aos="fade-up">
-
+                <div id="about" className="content" data-scrollview="true" data-aos="fade-up">
+                    <div className="bg"></div>
                     <div className="container" data-animation="true" data-animation-type="animate__fadeInDown">
                         <h2 className="content-title">Sobre mim</h2>
-
 
                         <div className="row">
 
@@ -102,7 +101,7 @@ export default function Ste(props) {
                                         </div>
                                         <div className="info">
                                             Dara Marques
-                                            <small>Psicóloga Clinica</small>
+                                            <small>Psicóloga</small>
                                         </div>
                                     </div>
                                 </div>
@@ -120,39 +119,39 @@ export default function Ste(props) {
                 <div id="team" className="content" data-scrollview="true">
                     <div className="container">
                         <div className="row">
+                            <div className="col-md-6 mb-2">
+                                <div className="sobre-text h-100">
+                                    <h2 className="content-title">
+                                        Especialidades </h2>
+                                    <p className="content-desc">
+                                        <p>
+                                            - Psicoterapia Individual através da Terapia Cognitivo Comportamental
+                                        </p>
+                                        <p>
+                                            - Dara Macedo Marques Hortolani - CRP: 06/164999
+                                        </p>
 
-                            <div className="col-6">
-
-
-
-
-                                <h2 className="content-title">
-                                    Especialidades </h2>
-                                <p className="content-desc">
-                                    <p>
-                                        - Psicoterapia Individual através da Terapia Cognitivo Comportamental
                                     </p>
-                                    <p>
-                                        - Dara Macedo Marques Hortolani - CRP: 06/164999
-                                    </p>
-
-                                </p>
-
+                                </div>
 
                             </div>
-                            <div className="col-6">
-                                <h2 className="content-title">
-                                    Qual é o foco do meu trabalho? </h2>
-                                <div className="content-desc">
-                                    <p>
-                                        Através dos atendimentos individuais, ajudo mulheres que buscam aprender a lidar com a ansiedade que normalmente está em torno do estresse, autocobrança, perfeccionismo, procrastinação, insegurança... e por ai vai!
-                                    </p>
-                                    <p>
+                            <div className="col-md-6">
 
-                                        Ajuda a compreender que a ansiedade faz parte de nós,  que é possível viver com ela com mais leveza,  não a vendo como uma inimiga, entenderem melhor a si mesmas com a psicoterapia e a partir disso, ir fazendo mudanças saudáveis e duradouras. É gratificante vê-las se descobrindo, sabendo lidar de uma forma mais saudável com seus sentimentos, mudando hábitos de comportamento e tendo uma vida mais leve!
-                                    </p>
+                                <div className="sobre-text h-100">
+                                    <h2 className="content-title">
+                                        Qual é o foco do meu trabalho? </h2>
+                                    <div className="content-desc">
+                                        <p>
+                                            Através dos atendimentos individuais, ajudo mulheres que buscam aprender a lidar com a ansiedade que normalmente está em torno do estresse, autocobrança, perfeccionismo, procrastinação, insegurança... e por ai vai!
+                                        </p>
+                                        <p>
 
+                                            Ajuda a compreender que a ansiedade faz parte de nós,  que é possível viver com ela com mais leveza,  não a vendo como uma inimiga, entenderem melhor a si mesmas com a psicoterapia e a partir disso, ir fazendo mudanças saudáveis e duradouras. É gratificante vê-las se descobrindo, sabendo lidar de uma forma mais saudável com seus sentimentos, mudando hábitos de comportamento e tendo uma vida mais leve!
+                                        </p>
+
+                                    </div>
                                 </div>
+
                             </div>
                         </div>
 
@@ -161,17 +160,17 @@ export default function Ste(props) {
                 </div>
 
 
-                <div id="service" className="content mt-0" data-scrollview="true">
-
+                <div id="service" className="content" data-scrollview="true">
+                    <div className="bg"></div>
                     <div className="container">
 
                         <h2 className="content-title">Vantagens do Atendimento On-line!</h2>
-                        <p className="content-desc">
+                       {/*  <p className="content-desc">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consectetur eros dolor,<br />
                             sed bibendum turpis luctus eget
-                        </p>
+                        </p> */}
 
-                        <div className="row">
+                        <div className="row sobre-text shadow">
 
                             <div className="col-lg-4 col-md-6">
                                 <div className="service">
@@ -200,7 +199,7 @@ export default function Ste(props) {
                                     <div className="icon" data-animation="true" data-animation-type="animate__bounceIn"><i className="fa fa-file"></i></div>
                                     <div className="info">
                                         <h4 className="title">Sigiloso</h4>
-                                        <p className="desc">O sigilo acontece da mesma forma que um atendimento presencial, mas nesse caso é importante que o paciente auxilie estando num ambiente que se sente confortável e seguro para que as informações não sejam ouvidas por outras pessoas;.</p>
+                                        <p className="desc">O sigilo acontece da mesma forma que um atendimento presencial, mas nesse caso é importante que o paciente auxilie estando num ambiente que se sente confortável e seguro para que as informações não sejam ouvidas por outras pessoas.</p>
                                     </div>
                                 </div>
                             </div>
@@ -265,7 +264,7 @@ export default function Ste(props) {
                                 if (i >= 8) return "";
 
                                 return <div key={i} className="col-lg-3 col-md-4 ">
-                                    <div className="work card shadow">
+                                    <div className="work">
                                         <div className="image">
                                             <a href={e.permalink}><img src={e.media_url} /></a>
                                         </div>
@@ -289,36 +288,36 @@ export default function Ste(props) {
 
 
 
-                <div id="contact" className="content bg-light" data-scrollview="true">
+                <div id="contact" className="content" data-scrollview="true">
 
                     <div className="container">
-                        <h2 className="content-title">Conteúdos</h2>
+                        <h2 className="content-title text-white">Conteúdos</h2>
                         <p className="text-center" >
                             Caso tenha interesse de receber conteúdos sobre psicologia toda semana, me siga nas redes sociais e se inscreva na minha newsletter.
                         </p>
 
                         <div className="row">
 
-                            <div className="col-lg-6">
+                            <div className="col-md-6">
 
 
                                 <div className="team">
                                     <div className="image" data-animation="true" data-animation-type="animate__flipInX">
-                                        <img className='saibamaispic' src="./saibamaispicture.png" alt="Ryan Teller" />
+                                        <img className='saibamaispic border-white' src="./saibamaispicture.png" alt="Ryan Teller" />
                                     </div>
                                     <div className="info">
                                         <p>Me siga nas redes sociais!</p>
-                                        <div className="social">
-                                            <a href="https://web.facebook.com/psidaramarques" target={"_blank"} ><i className="fab fa-facebook-f fa-lg fa-fw"></i></a>
-                                            <a href="https://www.instagram.com/psidaramarques/" target={"_blank"}><i className="fab fa-instagram fa-lg fa-fw"></i></a>
-                                            <a href="https://www.linkedin.com/in/daramarques/" target={"_blank"}><i className="fab fa-linkedin fa-lg fa-fw"></i></a>
+                                        <div className="social ">
+                                            <a href="https://web.facebook.com/psidaramarques" target={"_blank"} ><i className="fab fa-facebook-f fa-lg fa-fw text-white"></i></a>
+                                            <a href="https://www.instagram.com/psidaramarques/" target={"_blank"}><i className="fab fa-instagram fa-lg fa-fw text-white"></i></a>
+                                            <a href="https://www.linkedin.com/in/daramarques/" target={"_blank"}><i className="fab fa-linkedin fa-lg fa-fw text-white"></i></a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
 
 
-                            <div className="col-lg-6 form-col" data-animation="true" data-animation-type="animate__fadeInRight">
+                            <div className="col-md-6 form-col" data-animation="true" data-animation-type="animate__fadeInRight">
                                 <form className="form-horizontal">
                                     <h3 className="text-center mb-1">Newsletter</h3>
                                     <p className="text-center">
