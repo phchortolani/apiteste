bf730aefc764266b095823975bb03c5f3333be49
phchortolani <phchortolani@gmail.com>
2022-02-24 17:21:05 -0300

    add email

--- pages/api/sendemail.js
+++ pages/api/sendemail.js
@@ -10,41 +10,37 @@ export default function Sendemail(req, res) {
 
         const collection = db.collection("newsletter");
 
-        let ret = await collection.insertOne({ email: email.toLowerCase(), createAt: new Date() });
+        let ret = await collection.insertOne({ email: email, createAt: new Date() });
 
         if (ret) return true;
         else return false;
     }
 
+    if (save()) {
+        const client = new SMTPClient({
+            user: process.env.MAIL,
+            password: process.env.MAIL_PASS,
+            host: 'smtp.gmail.com',
+            ssl: true
+        });
 
-
-    const client = new SMTPClient({
-        user: process.env.MAIL,
-        password: process.env.MAIL_PASS,
-        host: 'smtp.gmail.com',
-        ssl: true
-    });
-
-    try {
-        async function send(destino) {
-            await client.sendAsync({
-                text: `Olá! Este e-mail: ${email} quer receber conteúdos toda a semana sobre psicologia.`,
-                from: process.env.MAIL,
-                to: destino,
-                subject: 'Solicitação de Newsletter!!',
-            });
+        try {
+            client.send(
+                {
+                    text: `Olá!, Este e-mail: ${email} quer receber conteúdos toda a semana sobre psicologia.`,
+                    from: process.env.MAIL,
+                    to: "phchortolani@gmail.com",
+                    subject: 'Solicitação de Newsletter!!',
+                }
+            )
+        }
+        catch (e) {
+            res.status(400).end(JSON.stringify({ message: "Error" }))
+            return;
         }
 
-        send("phchortolani@gmail.com");
-
-    }
-    catch (e) {
-        res.status(400).end(JSON.stringify({ message: "Error" }))
-        return;
-    }
-
-    res.status(200).end(JSON.stringify({ message: 'Send Mail' }))
-
+        res.status(200).end(JSON.stringify({ message: 'Send Mail' }))
+    } else res.status(400).end(JSON.stringify({ message: "Error" }))
 
 
 
