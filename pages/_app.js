4bfd63300526724c6e0469d1b59086506fa37ebf
phchortolani <phchortolani@gmail.com>
2022-03-14 11:23:00 -0300

    ajustes token

--- pages/_app.js
+++ pages/_app.js
@@ -29,7 +29,7 @@ function MyApp({ Component, pageProps }) {
                     href="https://fonts.googleapis.com/css?family=Poppins" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta name="google-site-verification" content="hpfneITc-QepH2PbgAZFYa-Y7rTJWSaVkXdJx3IZyFk" />
-                <link rel="icon" type="image/png" href="/dmlogolittle.png" />
+                <link rel="icon" type="image/png" href="./dmlogolittle.png" />
                 <title>Dara Marques Psicóloga</title>
                 <link
                     href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -38,7 +38,7 @@ function MyApp({ Component, pageProps }) {
                 />
                 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossOrigin="anonymous" />
 
-               {/*  <link rel="stylesheet" href="@sweetalert2/theme-material-ui/material-ui.css" /> */}
+                {/*  <link rel="stylesheet" href="@sweetalert2/theme-material-ui/material-ui.css" /> */}
 
                 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
