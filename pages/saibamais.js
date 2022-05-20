a044243400cd11bf0378a889e3fba1279e71d6d5
phchortolani <phchortolani@gmail.com>
2022-05-20 14:19:38 -0300

    refresh saibamais

--- pages/saibamais.js
+++ pages/saibamais.js
@@ -7,21 +7,22 @@ export default function SaibaMais() {
     }
     return (
 
-        <div className="containerSaibaMAis text-center text-white">
+        <div className="containerSaibaMAis text-center flowersBackGround text-primary-dara ">
+            <></>
             <div className='pb-20px'>
                 <img className='saibamaispic' src="./saibamaispicture.png"></img>
             </div>
             <h6><b>CRP </b>06/164999</h6>
             <div >
-            <h6>Dara Macedo Marques Hortolani</h6>
-               
+                <h6>Dara Macedo Marques Hortolani</h6>
+
             </div>
 
 
-            <a style={btnA} href="https://api.whatsapp.com/send?phone=5511978493885" target="_blank" type="button" className="btn btn-primary bg-none btn-block btn-large border-2 border border-white w-100 fw-bolder">Agende uma consulta</a>
+            <a style={btnA} href="https://api.whatsapp.com/send?phone=5511978493885" target="_blank" type="button" className="btn btn-primary btn-block btn-large saibamais-btn w-100 fw-bolder">Agende uma consulta</a>
             {/*  <button type="button" className="btn btn-primary btn-block btn-large w-100">Ver site</button> */}
-            <a style={btnA} href="/Blog" type="button" className="btn btn-primary btn-block btn-large w-100 bg-none border-2 border border-white  fw-bolder">Blog</a>
-            <a style={btnA} href="/" type="button" className="btn btn-primary btn-block btn-large w-100  bg-none border-2 border border-white fw-bolder">Site</a>
+            <a style={btnA} href="/Blog" type="button" className="btn btn-primary btn-block btn-large w-100 border-2 saibamais-btn fw-bolder">Blog</a>
+            <a style={btnA} href="/" type="button" className="btn btn-primary btn-block btn-large w-100  saibamais-btn fw-bolder">Site</a>
 
 
 
