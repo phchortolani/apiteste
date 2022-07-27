2b62a1fe0f7572ada1c417b442687586a8002d70
phchortolani <phchortolani@gmail.com>
2022-07-27 18:12:01 -0300

    get ip

--- pages/_document.js
+++ pages/_document.js
@@ -37,7 +37,7 @@ export default function Document() {
 
         </script>
 
-
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
       </Head>
       <body>
 
