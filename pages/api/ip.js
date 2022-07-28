ee3f9eb630e3628b9de9d83a8716fe2cf038f61a
phchortolani <phchortolani@gmail.com>
2022-07-28 15:17:04 -0300

    get ip

--- pages/api/ip.js
+++ pages/api/ip.js
@@ -7,11 +7,11 @@ export default async function ip(request,response){
    
     const { ipString } = request.body;
 
-    await fetch("http://ip-api.com/json/"+ipString+"?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,hosting,query")
+    await fetch("http://ip-api.com/json/"+ipString+"?fields=mobile,status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,hosting,query")
     .then(async function (data) {
         var res = await data.json()
         if (res) {
-            response.send(`País:${res.country} - Região: ${res.regionName} - ${res.region} - Cidade: ${res.city} - mobile:`+res.mobile) 
+            response.send(`País:${res.country} - Região: ${res.regionName} - ${res.region} - Cidade: ${res.city} - mobile: ${res.mobile}, - ISP: ${res.isp}`) 
         }
     });
 
