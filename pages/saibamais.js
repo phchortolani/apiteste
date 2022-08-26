367427fdc97c3b2311ac0246e6c4dc630e504abc
phchortolani <phchortolani@gmail.com>
2022-08-26 13:27:37 -0300

    agenda encerrada

--- pages/saibamais.js
+++ pages/saibamais.js
@@ -18,13 +18,10 @@ export default function SaibaMais() {
                     <p className="w-75 small">A Dara é psicóloga e atua na área clínica através da abordagem Cognitivo Comportamental.</p>
                 </div>
             </div>
-            <a style={fontsize} href="https://wa.me/message/OQPASIZH7O75G1" target="_blank" type="button" className="btn btn-primary btn-block btn-large saibamais-btn w-100 ">Agende uma consulta</a>
+            <a style={fontsize} href="https://wa.me/message/OQPASIZH7O75G1" target="_blank" type="button" className="btn btn-primary disabled btn-block btn-large saibamais-btn w-100 ">Agenda fechada!</a>
             {/*  <button type="button" className="btn btn-primary btn-block btn-large w-100">Ver site</button> */}
             <a  style={fontsize}  href="/Blog" type="button" className="btn btn-primary btn-block btn-large w-100 border-2 saibamais-btn">Blog</a>
             <a style={fontsize}  href="/" type="button" className="btn btn-primary btn-block btn-large w-100  saibamais-btn ">Site</a>
-
-
-
         </div>
     )
 }
